8ddf6ee6e1315f884aadfab72cb1e9918165e30d
tony <ashelto6@kent.edu>
2024-12-31 21:24:29 -0500

    lets serve html page

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 const app = express();
-const path = path();
+const vpath = path();
 
 
 app.use('/images', express.static(path.join(__dirname, '../images')));
