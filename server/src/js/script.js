92f61f7734de556c38ddeb3e1100ca43a584d01b
tony <ashelto6@kent.edu>
2026-07-10 23:36:23 -0700

    Reorganize server source into html/ and js/ subdirectories
    Moves index.html into src/html/ and server.js into src/js/, adds
    src/js/script.js, and updates server.js routing/paths and
    package.json to match the new layout. This is the boilerplate
    starting point for the app.
    
    Closes #3

---
+++ server/src/js/script.js
@@ -0,0 +1,6 @@
+console.log(Date());
+for(let i = 0; i < 5; i++)
+{
+    console.log(i+1);
+}
+console.log('JUJU the chosen Bun');
--- server/src/js/server.js
+++ server/src/js/server.js
@@ -2,15 +2,16 @@ const express = require('express')
 const path = require('path');
 const app = express();
 
+app.use('/images', express.static(path.join(__dirname, '../../images')));
 
-app.use('/images', express.static(path.join(__dirname, '../images')));
-
-
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'html', 'index.html'));
     console.log(`Request served by ${process.env.SERVER_HOST}`);
 });
 
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'script.js'));
+});
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`${process.env.SERVER_HOST} is listening on port ${process.env.SERVER_PORT}`);
